5caf811577435e86c116b782e7d63bdf0e2ce2d9
hrid1 <hridoycse1@gmail.com>
2024-11-08 12:07:12 +0600

    Setup done

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h3 className="">Hellow world</h3>
+    </>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: "class",
+  theme: {
+    extend: {
+      colors: {
+        primary: "#E58411",
+        secondary: "#1e1e1e",
+        "secondary-bg": "#F7f7f7",
+      },
+      fontFamily: {
+        popins: ["Poppins", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
